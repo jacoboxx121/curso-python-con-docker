a8d6e85c4e8c64ecf15775b9fbc9e275dbeba44c
jacoboxx121 <jacobocuevas68@gmail.com>
2025-07-12 10:25:37 -0300

    Delete python taller directory

--- python taller/entrada.py
+++
@@ -1,8 +0,0 @@
-#Entrada de datos por teclado
-
-nombre = input("Ingresa su nombre: ")
-apellido = input("Ingrese su apellido: ")
-print(f"Nombre {nombre}, Apellido {apellido}")
-
-edad = int(input("Ingrese su edad: "))
-print(f"Tu edad es: {edad + 1}")
